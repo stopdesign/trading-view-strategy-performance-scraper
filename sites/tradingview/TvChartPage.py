df72b6cd794ec863c8ea4fa9229789b6a84a4111
Konstantin Vankov <19534600+kocko94@users.noreply.github.com>
2022-08-07 15:01:34 +0100

    cleaning the table correctly

--- sites/tradingview/TvChartPage.py
+++ sites/tradingview/TvChartPage.py
@@ -1,5 +1,4 @@
-
-from selenium.common import StaleElementReferenceException
+from selenium.common import StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.by import By
 
 from driver.BaseDriver import BaseDriver
@@ -15,12 +14,10 @@ class TvChartPage(TvBasePage):
         self.accept_cookies()
         self.wait_for_chart_to_appear()
 
-    def login(self, user, password):
-        pass
-
     def wait_for_chart_to_appear(self):
         self.driver.wait_and_get_element(5, By.CLASS_NAME, "chart-widget")
         return self
+
     # def extract_data(self) -> DataFrame:
     #     self.driver.wait_and_get_element(5, By.CLASS_NAME, "tv-data-table")
     #     table_content = ScraperUtils.extract_tv_table_unwanted_info(self.driver)
@@ -33,19 +30,28 @@ class TvChartPage(TvBasePage):
         #     overlays = soup.find_all("div", {"data-name": "legend-source-item"})
         #     print(f"Overlays count: {len(overlays)}")
         #     return len(overlays) > 0
-
+        self.check_and_close_popups()
         chart = self.driver.wait_and_get_element(5, By.CLASS_NAME, "chart-widget")
         ActionChains(self.driver).context_click(chart).perform()
-        xpath = "//span[contains(text(), 'Remove indicators')]"
-        remove_indicators = self.driver.wait_and_get_element(5, By.XPATH, xpath)
+        overlays_table = self.driver.wait_and_get_element(5, By.ID, "overlap-manager-root")
+        remove_indicators = overlays_table.find_element(By.XPATH, "//span[contains(text(), 'Remove indicators')]")
+        reset_chart = overlays_table.find_element(By.XPATH, "//span[contains(text(), 'Reset chart')]")
         # https://stackoverflow.com/a/44914767
+        remove_indicators.click()
         try:
-            remove_indicators.click()
+            reset_chart.click()
         except StaleElementReferenceException as e:
             pass
         return self
 
-    def remove_chart_overlays(self):
-        chart = self.driver.wait_and_get_element(5, By.CLASS_NAME, "chart-widget")
-        ActionChains(self.driver).context_click(chart).perform()
-        self.driver.wait_and_get_element(5, By.XPATH, "//span[contains(text(), 'Remove indicators')]").click()
+    def check_and_close_popups(self):
+        try:
+            close_buttons = filter(
+                lambda button: "close-button" in button.get_attribute("class") or
+                               "closeButton" in button.get_attribute("class"),
+                self.driver.wait_and_get_element(1, By.ID, "overlap-manager-root").find_elements(By.TAG_NAME, "button"))
+            close_button = next(close_buttons, None)
+        except TimeoutException as e:
+            return
+        if close_button:
+            close_button.click()
