b4f1e34ada666fa591a8e3a9f6cdf525e1d99dca
Konstantin Vankov <19534600+kocko94@users.noreply.github.com>
2022-08-30 22:51:09 +0100

    Fetching TV scripts

--- network/BinanceClient.py
+++ network/BinanceClient.py
@@ -1,4 +1,6 @@
+
 import requests
+from requests import Response
 
 import EnvConfig
 
@@ -12,12 +14,16 @@ def request_all_symbols_perpetual() -> list:
 
 
 def __fetch_symbols(should_get_spot: bool = False) -> list:
-    proxies = {'https': 'http://192.168.0.86:8118',
-               'http': 'http://192.168.0.86:8118'} if __do_I_need_proxies() else None
     url_futures = "https://fapi.binance.com/fapi/v1/exchangeInfo"
     url_spot = "https://api.binance.us/api/v3/exchangeInfo"
-    r = requests.get(url_spot if should_get_spot else url_futures, proxies=proxies)
+    r = __make_binance_request(url_spot if should_get_spot else url_futures)
     return r.json()["symbols"]
+
+
+def __make_binance_request(url: str) -> Response:
+    proxies = {'https': 'http://192.168.0.86:8118',
+               'http': 'http://192.168.0.86:8118'} if __do_I_need_proxies() else None
+    return requests.get(url, proxies=proxies)
 
 
 def __do_I_need_proxies() -> bool:
