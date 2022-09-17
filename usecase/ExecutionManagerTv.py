39809033c4846c6930af617e9fff53751cd878f6
Konstantin Vankov <19534600+kocko94@users.noreply.github.com>
2022-09-17 08:40:43 +0100

    Small clean up

--- usecase/ExecutionManagerTv.py
+++ usecase/ExecutionManagerTv.py
@@ -5,60 +5,11 @@ from driver.ScraperDriver import ScraperDriver
 from model.ExecutionConfig import ExecutionConfig
 from model.RuntimeConfig import RuntimeConfig
 from network import PerformanceServerClient
-from network.PerformanceServerClient import NetworkError
+from network.PerformanceServerClient import NetworkError, RuntimeConfigExhaustedException
 from sites.tradingview.TvChartPage import TvChartPage
 from sites.tradingview.TvHomePage import TvHomePage
 from usecase import ProvideRuntimeConfig
-from utils import TimeUtils, FileUtils
-
-
-def obtain_strategy_performance_data_for(chart_page: TvChartPage,
-                                         execution_config: ExecutionConfig,
-                                         should_add_trades: bool = True) -> dict:
-    def __add_strategy_report_to(output: dict, overview_report: dict, trades_report: list):
-        strategy_name = strategy.name
-        if strategy.name not in output.keys():
-            output[strategy_name] = {"script": strategy.script}
-
-        coin_name = symbol.equity_name
-        interval_value = interval.value
-
-        if coin_name not in output[strategy_name].keys():
-            output[strategy_name][coin_name] = {}
-
-        report = overview_report
-        if len(trades_report) > 0:
-            report["trades"] = trades_report
-        output[strategy_name][coin_name][interval_value] = overview_report
-
-    logging.info(f"Obtaining performance data for "
-                 f"{len(execution_config.strategies)} strategies, "
-                 f"{len(execution_config.symbols)} symbols and "
-                 f"{len(execution_config.intervals)} time intervals.")
-
-    performance_report = {}
-    for strategy in execution_config.strategies:
-        with TimeUtils.measure_time("Obtaining stats for strategy " + strategy.name + " took {}."):
-            chart_page.clean_all_overlays() \
-                .add_strategy_to_chart(strategy.script) \
-                .change_footer_window_full_size(should_maximize_it=True)
-            for symbol in execution_config.symbols:
-                chart_page.change_symbol_to(symbol).remove_possible_advert_overlay()
-                with TimeUtils.measure_time("Obtaining stats for symbol " + symbol.equity_name + " took {}."):
-                    for interval in execution_config.intervals:
-                        chart_page.change_time_interval_to(interval).remove_possible_advert_overlay()
-                        strategy_overview_stats = chart_page.extract_strategy_overview_report()
-                        strategy_trades_stats = chart_page.extract_strategy_trades_report() if should_add_trades else []
-                        if strategy_overview_stats is not None:
-                            __add_strategy_report_to(performance_report, strategy_overview_stats, strategy_trades_stats)
-                            logging.info(
-                                f"Added report for {symbol.equity_name} and interval {interval.value}: "
-                                f"{strategy_overview_stats}")
-                            FileUtils.write_json(execution_config.output_file_name, performance_report)
-
-                        else:
-                            logging.info(f"No report found for {symbol.equity_name} and interval {interval.value}.")
-    return performance_report
+from utils import TimeUtils
 
 
 def setup_driver(headless: bool = False) -> ScraperDriver:
@@ -98,6 +49,8 @@ def start_obtaining_performances(execution_config: ExecutionConfig):
         except NetworkError as e:
             __clean_up_on_crash(e, driver, runtime_config)
             raise e
+        except RuntimeConfigExhaustedException as e:
+            logging.info(e)
         except Exception as e:
             __clean_up_on_crash(e, driver, runtime_config)
             start_obtaining_performances(execution_config)
